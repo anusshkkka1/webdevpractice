d3e989e53551faca160b7bda7aa48d3e50f31868
Anushka Sharma <anushkaasharma429@gmail.com>
2023-03-30 08:52:42 +0545

    signupform

--- script.js
+++ script.js
@@ -1,58 +1,32 @@
-//console.log("Javascript");
+let submitButton = document.getElementById("submitButton");
 
-//window.alert("hello");
+let errors={
+    
+}
 
-//window.confirm("are you sure you want to exit ?");
+submitButton.addEventListener("click",function(e){
+    e.preventDefault();
+    let signupForm =document.forms["signupForm"];
+    let firstNameInput=signupForm.firstname;
+    let lastNameInput=signupForm.lastname;//<input name="lastname">
+    
+    if(firstNameInput.value==""){
+        errors.firstName="firstname should not be empty";
+        
+        }
+        if(lastNameInput.value==""){
+            errors.lastName="lastname should not be empty";
+            
+        }
+        console.log(errors);
+        for(let error in errors){
 
-//prompt dialog box
-// let answer = window.prompt("What is the capital of Nepal?");
-// if (answer="Kathmandu");{ 
-// window.alert("you are correct");
-// }
-// console.log(answer);
+        }
+    });
+    // let lastnameGroup=document.getElementById('lastnameGroup');
+    // let lastnameMessage=document.querySelector("#lastnameGroup .message");
+    // lastnameGroup.classList.add("error");
+    // lastnameMessage.textContent="lastname should not be empty";
 
-//document
-//window.document.body.style.backgroundColor="skyblue";
-//document.body.style.fontFamily="sans-serif";
 
-//document.body,innerHTML="<h1>CSS</h1>";
 
-//selector
-//let movies=document.getElelemntbtID("movies");
-//movies.style.backgroundColor="cyan";
-//console.log(movies);
-
-//class selector
-//let movies= document.getElementsByClassName("moviesList")
-//console.log(movies);
-
-//query selector
-//let movies = document.querySelector(".moviesList")
-//movies.classList.add("big-red");
-//movies.classList.remove("big-red");
-//movies.classList.toggle("big-red");
-
-//query selectorall
-//let movies1 = document.querySelectorAll(".moviesList")
-//movies1[1].style.backgroundColor="blue";
-
-//event listener
-//document.addEventListener("click",function(){
-  //  window.alert("You clicked");
-//})
-
-//let button= document.querySelector('.click');
-//button.addEventListener("click",function(){
-  //  window.alert("You clicked a button");
-//})
-
-let button= document.querySelector('.click');
-button.addEventListener("click",function(){
-    document.body.classList.toggle("dark");
-    if (button.textContent=="Dark Mode"){
-        button.textContent="Light Mode";
-    }
-    else{
-        button.textContent="Dark Mode";
-    }
-});
